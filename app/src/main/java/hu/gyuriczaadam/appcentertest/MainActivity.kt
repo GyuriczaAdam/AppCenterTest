d53c2aaa4c25420ca6f4c77ce718fb247c6144b8
GyuriczaAdam <suhf.adam@gmail.com>
2022-10-08 13:38:22 +0200

    test commit

---
+++ app/src/main/java/hu/gyuriczaadam/appcentertest/MainActivity.kt
@@ -0,0 +1,50 @@
+package hu.gyuriczaadam.appcentertest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import hu.gyuriczaadam.appcentertest.ui.theme.AppCenterTestTheme
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppCenter.start(
+            application, "d717491a-dfce-4105-8407-cda72a551ec9",
+            Analytics::class.java, Crashes::class.java
+        )
+        setContent {
+            AppCenterTestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AppCenterTestTheme {
+        Greeting("Android")
+    }
+}
